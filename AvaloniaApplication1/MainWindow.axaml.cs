1cbfa2a9da9f2245cc17b2366a1324df2ae4e601
d1zzyy23 <d.lykke23@gmail.com>
2025-02-27 10:55:03 +0100

    Initial commit

---
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Reflection;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace AvaloniaApplication1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ViewHandler(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is string viewName)
+        {
+            string fullWindowName = $"{GetType().Namespace}.{viewName}";
+            var viewType = Assembly.GetExecutingAssembly().GetType(fullWindowName);
+            if (viewType != null && Activator.CreateInstance(viewType) is UserControl newContent)
+            {
+                ContentArea.Content = newContent;
+            }
+        }
+    }
+
+    private void ThemeToggled(object sender, RoutedEventArgs e)
+    {
+        var resources = Avalonia.Application.Current?.Resources;
+        bool toggled = ((ToggleButton)sender).IsChecked ?? false;
+
+        if (!toggled)
+        {
+            resources["PrimaryBackground"] = new SolidColorBrush(new Color(255, 22, 22, 22));
+            resources["PrimaryForeground"] = new SolidColorBrush(Colors.Gainsboro);
+            resources["PrimaryBackgroundColor"] = new Color(255, 22, 22, 22);
+            resources["PrimaryBackgroundColor2"] = new Color(255, 11,11,11);
+
+        }
+        else
+        {
+            resources["PrimaryBackground"] = new SolidColorBrush(Colors.Gainsboro);
+            resources["PrimaryForeground"] = new SolidColorBrush(new Color(255, 22, 22, 22));
+            resources["PrimaryBackgroundColor"] = Colors.Gainsboro;
+            resources["PrimaryBackgroundColor2"] = Colors.White;
+
+        }
+    }
+}
